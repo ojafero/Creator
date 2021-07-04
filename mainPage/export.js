16aa37a2496264c7e6b770f0fe881945a6c68c8d
mohib <mohibumirza@gmail.com>
2021-07-04 01:51:54 -0700

    export.js

---
+++ mainPage/export.js
@@ -0,0 +1,14 @@
+function downloadInnerHtml(filename, elId, mimeType) {
+    var elHtml = document.getElementById(elId).innerHTML;
+    var link = document.createElement('a');
+    mimeType = mimeType || 'text/plain';
+
+    link.setAttribute('download', filename);
+    link.setAttribute('href', 'data:' + mimeType  +  ';charset=utf-8,' + encodeURIComponent(elHtml));
+    link.click(); 
+}
+
+var fileName =  'test.html'; // You can use the .txt extension if you want
+
+
+// downloadInnerHtml(fileName, 'test', 'text/html');
