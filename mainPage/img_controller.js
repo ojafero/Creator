a8714ff5a2c4691f0cfe2bb0157ce24d410a8b48
mohib <mohibumirza@gmail.com>
2021-07-03 17:12:04 -0700

    Height/Width Input

--- mainPage/img_controller.js
+++ mainPage/img_controller.js
@@ -4,37 +4,47 @@ function createImageController(){
     const controlContainerDiv = document.querySelector("#more-controls");
 
     const imgInputPicker = createImageInputPicker();
-    addEventListenerImageInputPicker(imgInputPicker);
 
     const WHPicker = createWHPicker();
 
     controlContainerDiv.append( imgInputPicker );
     controlContainerDiv.append( WHPicker );
 
+    addEventListeners(imgInputPicker, WHPicker);
+
 }
 
 function createImageInputPicker(){
     const imageInputPicker = document.createElement('div');
     imageInputPicker.innerHTML = ('<input id="img-input-field" class="input is-rounded" type="text" placeholder="Add Image Link">');
-    return imageInputPicker.querySelector('#img-input-field');
+    return imageInputPicker;
 }
 
 function createWHPicker() {
     const imageHWInputBox = document.createElement('div');
-    imageHWInputBox.innerHTML = ('<input class="input is-small" type="text" placeholder="Height">');
-    imageHWInputBox.innerHTML += ('<input class="input is-small" type="text" placeholder="Width">');
+    imageHWInputBox.innerHTML = ('<input id="img-height-input" class="input is-small" type="text" placeholder="Height">');
+    imageHWInputBox.innerHTML += ('<input id="img-width-input" class="input is-small" type="text" placeholder="Width">');
 
     return imageHWInputBox;
 }
 
-function addEventListenerImageInputPicker(imgInputPicker) {
-    imgInputPicker.onkeyup = function() {
-        console.log('what is the current div clicked?');
-        console.log(currentDivOnCanvasClicked);
-        console.log("current div clicked ended");
-        console.log("currentDivOnCanvasClicked.src");
-        console.log(imgInputPicker.value);
-        currentDivOnCanvasClicked.src = imgInputPicker.value;
+function addEventListeners(imgInputPicker, WHPicker) {
+
+    const imgInputBox = imgInputPicker.querySelector('#img-input-field');
+
+    imgInputBox.onkeyup = function() {
+        currentDivOnCanvasClicked.src = imgInputBox.value;
+    }
+
+    const imgWidthInputBox = WHPicker.querySelector('#img-width-input');
+    const imgHeightInputBox = WHPicker.querySelector('#img-height-input');
+
+    imgWidthInputBox.onkeyup = function() {
+        currentDivOnCanvasClicked.style.width = imgWidthInputBox.value;
+    }
+
+    imgHeightInputBox.onkeyup = function() {
+        currentDivOnCanvasClicked.style.height = imgHeightInputBox.value;
     }
 }
 
